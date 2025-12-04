945e33b453628e6882c9a1941be6ad643d0f9ec8
delmath <delvincourtmathis@gmail.com>
2025-12-04 22:47:20 +0100

    add: activity button and ubuntu reskin

--- front/src/default_apps.tsx
+++ front/src/default_apps.tsx
@@ -1,84 +1,100 @@
-import { ReactElement } from 'react'
+import { ReactElement } from "react";
 
 // import { ProjectTemplate } from '@atoms/project_template/project_template.tsx';
 // import { Terminal } from '@molecules/terminal/terminal.tsx';
-import { Contact } from '@molecules/contact/contact.tsx';
-import { OkNestorProject, RootmeProject, ServerProject, WhatsWebProject } from '@projects/index.ts';
-import {Terminal} from "@molecules/terminal/terminal.tsx";
-
+import { Contact } from "@molecules/contact/contact.tsx";
+import {
+    OkNestorProject,
+    RootmeProject,
+    ServerProject,
+    WhatsWebProject,
+} from "@projects/index.ts";
+import { Terminal } from "@molecules/terminal/terminal.tsx";
 
 export interface IApp {
-    label: string,
-    id: string,
-    content: ReactElement | null,
-    iconKey: 'whatsweb' | 'whatsecosystem' | 'server' | 'rootme' | 'oknestor' | 'terminal' | 'contact' | 'document' | 'pages',
-    state: 0 | 1 | 2,
-    type: 'application' | 'project' | 'contact',
+    label: string;
+    id: string;
+    content: ReactElement | null;
+    iconKey:
+        | "whatsweb"
+        | "whatsecosystem"
+        | "server"
+        | "rootme"
+        | "oknestor"
+        | "terminal"
+        | "contact"
+        | "document"
+        | "pages";
+    state: 0 | 1 | 2;
+    type: "application" | "project" | "contact";
     onClick?: () => void;
 }
 
 export const defaultApps: IApp[][] = [
     [
         {
-            label: 'WhatsWeb',
-            id: 'whatsweb',
-            content: <WhatsWebProject key={'whatsweb'} />,
-            iconKey: 'whatsweb',
+            label: "WhatsWeb",
+            id: "whatsweb",
+            content: <WhatsWebProject key={"whatsweb"} />,
+            iconKey: "whatsweb",
             state: 2,
-            type: 'project'
+            type: "project",
         },
         {
-            label: 'Server',
-            id: 'server',
-            content: <ServerProject key={'server'} />,
-            iconKey: 'server',
+            label: "Server",
+            id: "server",
+            content: <ServerProject key={"server"} />,
+            iconKey: "server",
             state: 0,
-            type: 'project'
+            type: "project",
         },
         {
-            label: 'RootMe',
-            id: 'rootme',
-            content: <RootmeProject key={'rootme'} />,
-            iconKey: 'rootme',
+            label: "RootMe",
+            id: "rootme",
+            content: <RootmeProject key={"rootme"} />,
+            iconKey: "rootme",
             state: 0,
-            type: 'project'
+            type: "project",
         },
         {
-            label: 'OkNestor',
-            id: 'oknestor',
-            content: <OkNestorProject key={'oknestor'} />,
-            iconKey: 'oknestor',
+            label: "OkNestor",
+            id: "oknestor",
+            content: <OkNestorProject key={"oknestor"} />,
+            iconKey: "oknestor",
             state: 0,
-            type: 'project'
-        }
+            type: "project",
+        },
+        {
+            label: "Terminal",
+            id: "terminal",
+            content: <Terminal key={"contact"} />,
+            iconKey: "terminal",
+            state: 0,
+            type: "application",
+        },
     ],
     [
         {
-            label: 'Terminal',
-            id: 'terminal',
-            content: <Terminal key={'contact'} />,
-            iconKey: 'terminal',
+            label: "Contact",
+            id: "contact",
+            content: <Contact key={"contact"} />,
+            iconKey: "contact",
             state: 0,
-            type: 'application'
-        }
-    ],
-    [
-        {
-            label: 'Contact',
-            id: 'contact',
-            content: <Contact key={'contact'} />,
-            iconKey: 'contact',
-            state: 0,
-            type: 'contact'
+            type: "contact",
         },
         {
-            label: 'CV.pdf',
-            id: 'cv',
+            label: "CV.pdf",
+            id: "cv",
             content: null,
-            iconKey: 'document',
+            iconKey: "document",
             state: 0,
-            type: 'contact',
-            onClick: () => { if (window) window.open('https://cdn.whatsweb.fr/CV.pdf', '_blank')!.focus(); }
-        }
-    ]
-]
+            type: "contact",
+            onClick: () => {
+                if (window)
+                    window
+                        .open("https://cdn.whatsweb.fr/CV.pdf", "_blank")!
+                        .focus();
+            },
+        },
+    ],
+];
--- front/src/design-system/atoms/controls/controls.tsx
+++ front/src/design-system/atoms/controls/controls.tsx
@@ -1,15 +1,30 @@
-import './style.scss'
+import "./style.scss";
 
 interface IControlsProps {
-    onClose?: () => void | undefined,
-    onMinimise?: () => void | undefined,
-    onFullScreen?: () => void | undefined,
+    onClose?: () => void | undefined;
+    onMinimise?: () => void | undefined;
+    onFullScreen?: () => void | undefined;
 }
 
-export const Controls = ({onClose = undefined, onMinimise = undefined, onFullScreen = undefined}: IControlsProps) => {
-    return <div className="controls">
-        <div className={`controls-btn ${onClose ? 'active' : ''}`} onClick={onClose}></div>
-        <div className={`controls-btn ${onMinimise ? 'active' : ''}`} onClick={onMinimise}></div>
-        <div className={`controls-btn ${onFullScreen ? 'active' : ''}`} onClick={onFullScreen}></div>
-    </div>
-}
+export const Controls = ({
+    onClose = undefined,
+    onMinimise = undefined,
+    onFullScreen = undefined,
+}: IControlsProps) => {
+    return (
+        <div className="controls">
+            <div
+                className={`controls-btn ${onMinimise ? "active" : ""}`}
+                onClick={onMinimise}
+            ></div>
+            <div
+                className={`controls-btn ${onFullScreen ? "active" : ""}`}
+                onClick={onFullScreen}
+            ></div>
+            <div
+                className={`controls-btn ${onClose ? "active" : ""}`}
+                onClick={onClose}
+            ></div>
+        </div>
+    );
+};
