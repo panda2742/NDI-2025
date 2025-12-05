e8122a1b34d7139f806133f167519fca17a0c2d3
Liammmmmmmm <lilefebv@student.42lyon.fr>
2025-12-05 04:01:42 +0100

    clean a lot of useless code

--- front/src/design-system/projects/index.ts
+++ front/src/design-system/projects/index.ts
@@ -1,17 +1,7 @@
-import { OkNestorProject } from "@projects/OkNestor/ok_nestor.tsx";
-import { RootmeProject } from "@projects/RootMe/rootme.tsx";
-import { ServerProject } from "@projects/Server/server.tsx";
-import { WhatsEcoSystemProject } from "@projects/WhatsEcoSystem/whatsecosystem.tsx";
-import { WhatsWebProject } from "@projects/WhatsWeb/whatsweb.tsx";
 import { Clipouille } from "@projects/Clipouille/Clipouille.tsx";
 import { SnakeProject } from "@projects/Snake/snake.tsx";
 
 export {
-    OkNestorProject,
-    RootmeProject,
-    ServerProject,
-    WhatsEcoSystemProject,
-    WhatsWebProject,
     Clipouille,
     SnakeProject,
 };
